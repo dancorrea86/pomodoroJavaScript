537cedf436391776b31ccd15ebd09cddbfb04651
dancorrea86 <danielmacielcorrea@gmail.com>
2019-11-24 18:55:53 -0300

    comite

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-let minutos = 00
+let minutos = 2
 let segundos = 10
 let tempo = minutos + ":" + segundos
 let clock = document.getElementById("clock")
 clock.innerHTML = tempo
+let sec = 60
 let funciona = true;
 
 function start() {
-    while(funciona || minutos == 0 || segundos != 0) {
+    funciona = true;
+    console.log("run")
+    var relogio = setInterval(function() {
+      if (minutos == 0 || funciona == false) {
+      clearInterval(relogio)
+    } else {
       contadorRelogio()
-    }
+    }}, 1000) 
 }
 
 function contadorRelogio() {
-  segundos -= 1; clock.innerHTML = segundos
+  console.log(clock, minutos)
+  for(minutos; minutos==0; minutos--) {
+    console.log(minutos)
+    for(segundos; segundos==0; segundos--) {
+      console.log(clock)
+      clock.innerHTML = minutos + ":" + segundos
+    }
+  }
 }
 
 function cancela() {
+  console.log("parado")
   funciona = false
 }
